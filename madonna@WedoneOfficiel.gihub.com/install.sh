9dd473bd029ea851ee72f32260ae7827bfdf4c65
Wedone <110472725+WedoneOfficiel@users.noreply.github.com>
2023-01-08 11:23:58 +0100

    Delete install.sh

--- madonna@WedoneOfficiel.gihub.com/install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-mkdir -p $HOME/.local/share/gnome-shell/extensions/perso@aaaaadrien.github.com
-
-rsync -avzh --delete ./perso@aaaaadrien.github.com/ $HOME/.local/share/gnome-shell/extensions/perso@aaaaadrien.github.com/
-
